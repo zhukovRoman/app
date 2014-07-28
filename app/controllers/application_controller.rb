77c328bf3cbb346c9918773aa5a751c4ac691f4e
roman <zhrovl@gmail.com>
2014-07-28 13:35:57 +0400

    add raise error from cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,4 +31,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     request.referrer
   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, :alert => exception.message
+  end
 end
