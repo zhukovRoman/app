2293ca22035e9b18adb97261a1b4b92e1743bc35
roman <zhrovl@gmail.com>
2014-08-04 20:31:49 +0400

    add rights for new user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,9 +5,9 @@ class Ability
     user ||= User.new # guest user (not logged in)
 
     puts user
-    can :index, ApartmentController if [1, 2, 3].include? user.id
-    can :index, EmployeeController if [1,2].include? user.id
-    can :index, ObjectController if [1,2].include? user.id
+    can :index, ApartmentController if [1, 2, 3, 4].include? user.id
+    can :index, EmployeeController if [1,2,4].include? user.id
+    can :index, ObjectController if [1,2,4].include? user.id
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
