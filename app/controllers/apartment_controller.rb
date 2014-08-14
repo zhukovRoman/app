47ccf35c8ab93ccb59bc2e2ce381446de37273f1
roman <zhrovl@gmail.com>
2014-08-14 18:10:06 +0400

    crm-json

--- app/controllers/apartment_controller.rb
+++ app/controllers/apartment_controller.rb
@@ -13,6 +13,51 @@ class ApartmentController < ApplicationController
   def index
     #puts current_user.inspect()
     authorize! :index, self
+    data = ''
+    file = File.new("test.json", "r")
+    while (line = file.gets)
+      data += line
+    end
+    file.close
+    require 'json'
+    data = JSON.parse(data)
+    puts data['GetBuildingGroupsResult']['date']
     render "index2"
+  end
+
+  def crm
+    token = ApartmentController.get_token
+    if (token==nil )
+      render plain: 'error (get_token)'
+    else
+      data = ApartmentController.get_apartments_json(token)
+      File.open('test.json', 'w') { |file| file.write(data.to_s) }
+      render plain: "ok"
+    end
+  end
+
+  private
+  def self.get_token
+    require 'net/http'
+    require 'json'
+
+    uri = URI('http://172.20.10.10:91/BuildingGroupService.svc/Authenticate?oauth_consumer_key=test')
+    resp = Net::HTTP.get(uri) # => String
+    result = JSON.parse(resp)
+    if result == nil
+      return nil
+    end
+    return result['AuthenticateResult']
+  end
+
+  def self.get_apartments_json (token)
+    url_path = 'http://172.20.10.10:91/BuildingGroupService.svc/GetBuildingGroups?oauth_consumer_key=test&hash_key='+token.to_s
+    uri = URI(url_path)
+    resp = Net::HTTP.get(uri) # => String
+    result = JSON.parse(resp)
+    if result == nil
+      return nil
+    end
+    return result
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
   get 'apartment/index2'
   get 'api/getchart'
 
+  get 'apartment/crm'
 
   get 'object/index'
   # The priority is based upon order of creation: first created -> highest priority.
