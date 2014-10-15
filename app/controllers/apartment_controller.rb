40a70ab03bd97c9f2416f073f40b0ab5320bfe00
roman <zhrovl@gmail.com>
2014-10-15 17:13:55 +0400

    fix rights

--- app/controllers/apartment_controller.rb
+++ app/controllers/apartment_controller.rb
@@ -1,5 +1,5 @@
 class ApartmentController < ApplicationController
-  before_filter :authenticate_user!
+  #before_filter :authenticate_user!
   before_action :change_partName
 
   def change_partName
