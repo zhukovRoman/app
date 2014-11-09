bd671c8d464d944839a2f4034f2468585c2554d3
roman <zhrovl@gmail.com>
2014-11-09 22:13:27 +0300

    fill vacancies table for month

--- app/models/vacancies.rb
+++ app/models/vacancies.rb
@@ -2,4 +2,13 @@ class Vacancies < ActiveRecord::Base
   belongs_to :department, inverse_of: :vacancies
 
   validates :count, numericality: { only_integer: true, :message =>  " должно быть целым числом!" }
+
+  def self.fill_empty_vac (date)
+    Department.where(parent_id:  nil).each do |dep|
+      vac = dep.vacancies.where(for_date: date.at_beginning_of_month..date.at_end_of_month).take
+      if (vac == nil)
+        dep.vacancies.create(count: 0, for_date: date.at_beginning_of_month+14.day)
+      end
+    end
+  end
 end
